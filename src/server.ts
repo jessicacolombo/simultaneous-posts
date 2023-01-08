a5aac3a7453e86dbac459ef92bcbdc7cba8fb2f3
jessicacolombo <jessicacolombo384@gmail.com>
2023-01-08 03:05:43 -0300

    add config to deploy app

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import app, { httpServer } from "./app";
 import AppDataSource from "./data-source";
+import "dotenv/config";
 
 (async () => {
   await AppDataSource.initialize().catch((err) => {
     console.error("Error during Data Source initialization", err);
   });
 
-  httpServer.listen(3000, () => {
+  httpServer.listen(process.env.PORT, () => {
     console.log("Servidor executando");
   });
 })();
